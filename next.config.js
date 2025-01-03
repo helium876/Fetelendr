b583cdaa1353bd462c76c4359e909b2b596108d7
helium876 <masters2020@gmail.com>
2025-01-03 17:36:38 -0500

    fix: Update CSP to allow Google Fonts

--- next.config.js
+++ next.config.js
@@ -40,7 +40,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://www.googleapis.com;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://www.googleapis.com;"
           }
         ]
       }
